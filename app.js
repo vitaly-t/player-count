670a12d331c054bcc1d479a5abcc788f1a49606a
rob <robgrovesemail@gmail.com>
2016-06-08 23:06:38 -0700

    Game database appears to have been fully and correctly populated. Tomorrow I will loop over each entry in the database and update their 'count' field. After that, I plan to setup different update schedules for games based on their current player base, as there are about 26,000 games and I can only make 100,000 per day.

--- app.js
+++ app.js
@@ -15,50 +15,20 @@ app.set('view engine', 'pug');
 // Prettify JSON
 app.set('json spaces', 2);
 
-function getPairs(data){
-  var pairs = "";
-  data.forEach(function(datum){
-    pairs += "(" + datum[0] + "," + datum[1] + "),";
-  });
-  pairs = pairs.substring(0,pairs.length-1);
-  return pairs;
-}
-
 app.get('/', function(req, res){
   //var url = 'https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?key=' + key + '&appid=440';
-  var url = 'http://api.steampowered.com/ISteamApps/GetAppList/v2/?key=' + key;
-
-  request.get(url, function(err, apiReq, apiRes){
-    if(err) throw err;
-    var apps = JSON.parse(apiRes).applist.apps;
-    var games = [];
-    var invalidDescriptors = ["trial", "key", "demo", "trailer", "dlc", "skins", "pack"];
-    for(var k = 0; k < apps.length; k++){
-      // Check if the name does NOT contain any invalid descriptors
-      if(!invalidDescriptors.some(function(descriptor){ return apps[k].name.toLowerCase().indexOf(descriptor) != -1;})){
-        games.push([apps[k].appid, "'" + apps[k].name.replace("'","''") + "'"]);
+  db.any("SELECT * FROM player_counts", [true])
+    .then(function(data){
+      res.json(data);
+    })
+    .catch(function(err){
+      if(err){
+        done();
+        console.log(err);
+        res.status(500).json({success:false, data:err});
       }
-    }
-		function factory(index) {
-			if (index < games.length) {
-          console.log(index);
-					return this.query('insert into player_counts(appid,name) values($1,$2)', games[index]);
-			}
-		}
-
-		db.tx(function () {
-				return this.sequence(factory);
-		})
-			.then(function (data) {
-        return res.send("SUCCESS!");
-			})
-			.catch(function (error) {
-				done();
-				console.log(error);
-				return res.status(500).json({success:false});
-			});
+    });
   
-  });
 });
 
 
---
+++ models/populate-all-games.js
@@ -0,0 +1,43 @@
+var url = 'http://api.steampowered.com/ISteamApps/GetAppList/v2/?key=' + key;
+
+request.get(url, function(err, apiReq, apiRes){
+  if(err) throw err;
+  var apps = JSON.parse(apiRes).applist.apps;
+  var games = [];
+  var invalidDescriptors = ["trial", "key", "demo", "trailer", "dlc", "skins", "pack"];
+  for(var k = 0; k < apps.length; k++){
+    // Check if the name does NOT contain any invalid descriptors
+    if(!invalidDescriptors.some(function(descriptor){ return apps[k].name.toLowerCase().indexOf(descriptor) != -1;})){
+      games.push([apps[k].appid, "'" + apps[k].name.replace("'","''") + "'"]);
+    }
+  }
+
+  // NOTE: Both the 'factory' function and 'db.tx ...' are VERY important.
+  // Initially, I had a lot of issues trying to do a bulk insert of records.
+  // Previous methods/modules (like 'pg') either would not accept a single
+  // massive query (here was about 26,000 records I was inserting) or would
+  // push individual queries into a queue that could be maxed out (attempting
+  // to add queries to a maxed out queue would result in them being ignored).
+  // As such, I've since switched over to 'pg-promise'.
+  // It allows for code like the below which lets you construct a sequence of
+  // queries to eventually be executed in full.
+  function factory(index) {
+    if (index < games.length) {
+        console.log(index);
+        return this.query('insert into player_counts(appid,name) values($1,$2)', games[index]);
+    }
+  }
+
+  db.tx(function () {
+      return this.sequence(factory);
+  })
+    .then(function (data) {
+      return res.send("SUCCESS!");
+    })
+    .catch(function (error) {
+      done();
+      console.log(error);
+      return res.status(500).json({success:false});
+    });
+
+});
