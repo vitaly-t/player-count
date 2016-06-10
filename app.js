bafa467d8e01bd0e188f54d5ce7b7f7bed9e5034
rob <robgrovesemail@gmail.com>
2016-06-10 16:44:49 -0700

    I have added 'pg-query-stream' and 'JSONStream' to the project. Using these modules in conjunction with 'pg-promise' allows for responses to the database from a query to be streamed back, rather than returned in bulk upon completion. By piping this stream to the res stream, we can quickly display huge amounts of data to the user.

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ var path = require('path');
 var request = require('request');
 var pgp = require('pg-promise')();
 
+// STREAMING
+var QueryStream = require('pg-query-stream');
+var JSONStream = require('JSONStream');
+var qs = new QueryStream("SELECT * FROM player_counts");
+
 var apiInfo = require('./config/api');
 var key = apiInfo.key;
 
@@ -18,17 +23,27 @@ app.set('json spaces', 2);
 app.get('/', function(req, res){
   //var url = 'https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?key=' + key + '&appid=440';
   res.write("Retrieving game data ... ");
-  db.any("SELECT * FROM player_counts", [true])
+  db.stream(qs, function(s){
+    s.pipe(JSONStream.stringify()).pipe(res);
+  })
     .then(function(data){
-      res.end(data);
+      console.log("Total rows processed:", data.processed);
+      console.log("Duration in milliseconds:",data.duration);
     })
     .catch(function(err){
-      if(err){
-        done();
-        console.log(err);
-        res.status(500).json({success:false, data:err});
-      }
+      console.log("ERROR", err.message || error);
     });
+//  db.any("SELECT * FROM player_counts", [true])
+//    .then(function(data){
+//      res.end(data);
+//    })
+//    .catch(function(err){
+//      if(err){
+//        done();
+//        console.log(err);
+//        res.status(500).json({success:false, data:err});
+//      }
+//    });
   
 });
 
