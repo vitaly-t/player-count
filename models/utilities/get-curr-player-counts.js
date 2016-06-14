98f9271faacd8069d7ad5ab6af18694508df333f
rob <robgrovesemail@gmail.com>
2016-06-13 23:39:51 -0700

    Added 'async' module to project. Requests for updated player counts are now made via 'asunc.map'. Async requests to the API provides a massive speed boost.

--- models/utilities/get-curr-player-counts.js
+++ models/utilities/get-curr-player-counts.js
@@ -1,23 +1,26 @@
 var request = require('request');
+var async = require('async');
 var db = require('../../config/db');
 var apiInfo = require('../../config/api');
 
 function getUpdatedCounts(games, cb){
   var URL = 'https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?key=' + apiInfo.key + '&appid=';
-  var newCounts = [];
-  games.forEach(function(game){
-    setTimeout(function(){
-      request(URL + game.appid, function(err, apiReq, apiRes){
-        if(err){
-          return cb(err);
-        }
-        newCounts.push(JSON.parse(apiRes));
-        if(newCounts.length === games.length){
-          return cb(null, newCounts);
-        }
-      });
-    }, 50);
-  });
+  var gameURLs = games.map(function(game){ return URL + game.appid; });
+  var count = 0;
+  async.map(gameURLs, function(url, callback){
+    console.error(++count);
+    request(url, function(err, apiReq, apiRes){
+      callback(err, JSON.parse(apiRes));
+    });
+  }, 
+    function(err, results){
+      console.error(results);
+      if(err){
+        return cb(err);
+      }
+      return cb(null, results);
+    }
+  );
 }
 
 module.exports = getUpdatedCounts;
--- spec/getCurrPlayerCounts.spec.js
+++ spec/getCurrPlayerCounts.spec.js
@@ -1,6 +1,20 @@
 describe("Testing function to retrieve current player counts for games given by array of appids.", function(){
   var getCurrPlayerCounts = require('../models/utilities/get-curr-player-counts');
   var getHighPopGames = require('../models/utilities/get-high-pop-games');
+  var originalTimeout;
+  var highPopGames;
+  
+  beforeAll(function(done){
+    getHighPopGames(function(err, games){
+      highPopGames = games;
+      done();
+    });
+  });
+
+  beforeEach(function(){
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
+  });
 
   it("Should gracefully errors returned by the request.", function(){
     
@@ -11,13 +25,15 @@ describe("Testing function to retrieve current player counts for games given by 
   });
 
   it("Should return an array of objects each containing the game's appid and current player count.", function(done){
-    getHighPopGames(function(err, games){
-      getCurrPlayerCounts(games, function(err, data){
-        console.log(data);
-        expect(err).toBe(null);
-        expect(Array.isArray(data)).toBe(true);
-        done();
-      });
+    getCurrPlayerCounts(highPopGames, function(err, data){
+      console.error(data);
+      expect(err).toBe(null);
+      expect(Array.isArray(data)).toBe(true);
+      done();
     });
+  });
+
+  afterEach(function(){
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
   });
 });
