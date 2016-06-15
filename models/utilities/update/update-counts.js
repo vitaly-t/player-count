6a8084ab12509a39cf5670576898ddfdf8f1a31a
rob <robgrovesemail@gmail.com>
2016-06-14 17:52:21 -0700

    Added function to perform updating. Begining tests now.

--- models/utilities/update/update-counts.js
+++ models/utilities/update/update-counts.js
@@ -6,36 +6,22 @@ require('rootpath')();
 var apiInfo = require('config/api');
 var db = require('config/db');
 
-function updateCounts(){
-  var curr = [];
-  var updated = [];
-  db.any("SELECT appid FROM player_counts WHERE count > 100")
-    .then(function(data){
-      curr = data;
-      checkAgainstSteam();
-    })
-    .catch(function(err){
-      throw err;
-    });
-
-  function checkAgainstSteam(){
-    var URL = 'https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?key=' + apiInfo.key + '&appid=';
-    var requests = [];
-    // IT IS POSSIBLE TO PIPE REQUESTS TO OTHER REQUESTS!
-    // Plan will be to make a request to Steam's API.
-    // Upon making a successful request, pipe the result to an API endpoint on
-    // our end (via 'PUT') which will then update the database with the
-    // information.
-    request(URL + appid).pipe(request.put("http://localhost:8080/api/updateCounts"));
-
-    
-  }
-
-  function makeRequest(){
-
-  }
-
-
+function updateCounts(gamesToUpdate){
+	db.tx(function (t) {
+		gamesToUpdate = gamesToUpdate.map(function(game){
+			return t.none("UPDATE player_counts SET count=$1 WHERE appid=$2", game.response.player_count, game.appid);
+		});
+		// this = t = transaction protocol context;
+		// this.ctx = transaction config + state context;
+		return t.batch(gamesToUpdate);
+	})
+	.then(function (data) {
+		console.log("Records successfully updated!");
+			// success;
+	})
+	.catch(function (error) {
+			console.log("ERROR:", error.message || error);
+	});
 }
 
 module.exports = updateCounts;
