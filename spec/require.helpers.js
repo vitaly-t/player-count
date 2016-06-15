98439367a0102fae6b0751e0c472cdf856eac033
rob <robgrovesemail@gmail.com>
2016-06-14 17:34:12 -0700

    Cleared require.helpers.js as it was made obsolete by 'rootpath' module. May use later.

--- spec/require.helpers.js
+++
@@ -1,7 +0,0 @@
-var getCurrPlayerCounts = require('../models/utilities/get/get-curr-player-counts');
-var getHighPopGames = require('../models/utilities/get/get-high-pop-games');
-
-module.exports = {
-  getCurrPlayerCounts: getCurrPlayerCounts,
-  getHighPopGames: getHighPopGames
-};
