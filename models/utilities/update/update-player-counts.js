280b7d77a5e66a4e0c8289d895f2d4f1a435c821
rob <robgrovesemail@gmail.com>
2016-06-15 15:53:26 -0700

    Changed 'update' field in player_counts database to be an array of timestamps. The 'updatePlayerCount' function now also appends the current timestamp to the 'updated' array. I'll eventually want to set a cap on the size of this array.

--- models/utilities/update/update-player-counts.js
+++ models/utilities/update/update-player-counts.js
@@ -11,12 +11,15 @@ function updatePlayerCounts(gamesToUpdate, cb){
     return cb(new Error("First parameter must be an array."));
   }
 	db.tx(function (t) {
-		gamesToUpdate = gamesToUpdate.map(function(game){
+		var updates = gamesToUpdate.map(function(game){
 			return t.none("UPDATE player_counts SET count=$1 WHERE appid=$2", [game.response.player_count, game.appid]);
 		});
+    var inserts = gamesToUpdate.map(function(game){
+      return t.none("UPDATE player_counts SET updated = array_append(updated,$1) WHERE appid=$2", [new Date(), game.appid]);
+    });
 		// this = t = transaction protocol context;
 		// this.ctx = transaction config + state context;
-		return t.batch(gamesToUpdate);
+		return t.batch(updates.concat(inserts));
 	})
 	.then(function (data) {
 		console.log("Records successfully updated!");
