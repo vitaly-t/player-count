9be1fe88c4288cac9a25a26135e3ebe10d4dbb52
rob <robgrovesemail@gmail.com>
2016-06-15 17:59:21 -0700

    Added 'browserify' and reformated js files.

--- public/javascripts/canvas.js
+++ public/javascripts/canvas.js
@@ -1,11 +1,12 @@
 (function canvasSetup(){
+  var interpPoly = require('./interp-poly.js');
   
   var points = [{x:20,y:40}, {x:60,y:20}, {x:100,y:120}, {x:160,y:10}];
 
   var c = document.getElementById("my-canvas");
   var ctx = c.getContext("2d");
-  var poly = getNewtonPoly(points);
-  var bounds = getBounds(points);
+  var poly = interpPoly.generatePoly(points);
+  var bounds = interpPoly.getBounds(points);
 
   ctx.beginPath();
   ctx.moveTo(points[0].x,points[0].y);
---
+++ public/javascripts/main.js
@@ -0,0 +1,6 @@
+(function() {
+
+  require('./interp-poly.js');
+  require('./canvas.js');
+
+})();
