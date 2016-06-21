822e05681d2f34436e5c3f5a8cb3fd02aad8b710
rob <robgrovesemail@gmail.com>
2016-06-21 16:26:04 -0700

    Working on JS for 'Last 30 Days' bargraph.

--- public/javascripts/bargraph-onhover.js
+++ public/javascripts/bargraph-onhover.js
@@ -1,6 +1,7 @@
 (function setupBargraphOnhover(){
   var bars = document.getElementsByClassName('bargraph-bar');
   var bargraphs = document.getElementsByClassName('bargraph');
+  var dailyPlayers = document.getElementById('daily-players');
 
   // Remember that the object returned by document.getElementsByClassName is
   // NOT an array. It is an HTML Collection, hence why we do the following:
@@ -16,16 +17,19 @@
     var appid = e.target.getAttribute('data-appid');
     var count = e.target.getAttribute('data-count');
     var time = e.target.getAttribute('data-time');
+    var display = document.getElementById(appid);
     time = new Date(time);
     time = time.toDateString();
-    var display = document.getElementById(appid);
-    display.innerHTML = count + " players " + "</br>" + " on " + time;
+    dailyPlayers.innerHTML = time;
+
+    display.innerHTML = count;
   }
 
   function barMouseOutHandler(e){
     var appid = e.target.getAttribute('data-appid');
     var display = document.getElementById(appid);
     display.innerHTML = "";
+    dailyPlayers.innerHTML = "Daily Players";
   }
 
 })();
