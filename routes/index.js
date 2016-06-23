424198f4400d92c41964e0681fb355d76c29821d
rob <robgrovesemail@gmail.com>
2016-06-23 00:43:18 -0700

    Added 'Top Games All Time' table to main page.

--- routes/index.js
+++ routes/index.js
@@ -27,6 +27,7 @@ var svgDims = require('../config/global').svgDims;
 router.get('/', function(req,res){
   var getTrending = require('../functions/get-trending');
   var get30DayAvg = require('../functions/get-30-day-avg');
+  var topGames = cache.get("topGames");
   var total = 0;
   var games = cache.get("highPopGames").map(function(game){
     game.avg = get30DayAvg(game.count);
@@ -40,7 +41,7 @@ router.get('/', function(req,res){
     return game;
   });
   var trending = getTrending(games);
-  res.render('index', {total: total, trending: trending, games:games.slice(0,10), svgDims: svgDims});
+  res.render('index', {total: total, trending: trending, games:games.slice(0,10), topGames: topGames, svgDims: svgDims});
 });
 
 router.get('/test', function(req, res){
