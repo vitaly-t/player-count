80519f5cdc9ea3a606fd2adaa7bdd050a9734c3f
rob <robgrovesemail@gmail.com>
2016-07-08 22:58:29 -0700

    Added 'legend-color-*' class. Selected games in 'compare' view now have a box next to them with their color in the plot.

--- public/javascripts/compare-input.js
+++ public/javascripts/compare-input.js
@@ -1,39 +1,40 @@
 (function comparePageSetup() {
   var cmpInput = document.getElementById('newComparison');
-  var cmpImg = document.getElementById('game-selection-img');
-  var found = null;
+  if(cmpInput){
+    var cmpImg = document.getElementById('game-selection-img');
+    var found = null;
 
-  // Want to use 'keyup' rather than 'keypress', as the latter are fired BEFORE
-  // the value of the key is added to the  input.
-  cmpInput.addEventListener('keyup', function(e) {
-    console.log(cmpInput.value);
-    if (cmpInput.value.length > 3) {
-      if (found === null || (found && found.name.indexOf(cmpInput.value) === -1)) {
-        $.getJSON('/api/partialSearch/?search=' + cmpInput.value, function(match) {
-          if (match !== null) {
-            cmpImg.src = '../images/' + match.appid + ".jpg";
-            found = match;
-          } else {
-            cmpImg.src = '';
-          }
-        });
+    // Want to use 'keyup' rather than 'keypress', as the latter are fired BEFORE
+    // the value of the key is added to the  input.
+    cmpInput.addEventListener('keyup', function(e) {
+      console.log(cmpInput.value);
+      if (cmpInput.value.length > 3) {
+        if (found === null || (found && found.name.indexOf(cmpInput.value) === -1)) {
+          $.getJSON('/api/partialSearch/?search=' + cmpInput.value, function(match) {
+            if (match !== null) {
+              cmpImg.src = '../images/' + match.appid + ".jpg";
+              found = match;
+            } else {
+              cmpImg.src = '';
+            }
+          });
+        }
       }
-    }
-    if (e.keyCode === 13) {
-      if (found !== null && appids.indexOf(found.appid) === -1) {
-        appids = appids.concat(found.appid);
-        window.location = 'http://localhost:8080/compare/' + appids.join(',');
-        return false;
+      if (e.keyCode === 13) {
+        if (found !== null && appids.indexOf(found.appid) === -1) {
+          appids = appids.concat(found.appid);
+          window.location = 'http://localhost:8080/compare/' + appids.slice(-4).join(',');
+          return false;
+        }
       }
-    }
-    return true;
-  });
-  cmpInput.addEventListener('keydown', function(e) {
-    if (e.keyCode == 46 || e.keyCode == 8) {
-      cmpImg.src = '';
-      found = null;
-    }
-  });
-
+      return true;
+    });
+    cmpInput.addEventListener('keydown', function(e) {
+      if (e.keyCode == 46 || e.keyCode == 8) {
+        cmpImg.src = '';
+        found = null;
+      }
+    });
+  }
 
 })();
